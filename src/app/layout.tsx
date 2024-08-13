4adda988b92bd98ca094a687dbd66d1ecd21f37b
prathameshpanse51 <93512551+prathameshpanse51@users.noreply.github.com>
2024-08-14 01:47:11 +0530

    started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import localFont from "next/font/local";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { Boxes } from "@/components/ui/background-boxes";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+// const fontSans = FontSans({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({ src: "../../public/Poppins/Poppins-Medium.ttf" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={(cn("min-h-screen bg-background"), myFont.className)}
+        suppressHydrationWarning={true}
+      >
+        <ShootingStars />
+        <StarsBackground />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
